9b01248cc9f774a6a467a1d214de15e5404dde1b
serii <seriiburduja@gmail.com>
2017-01-02 13:11:24 +0200

    finish main-menu

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,5 +1,8 @@
 (function($) {
-    
-
-
+   $('#show-socials').on('click', function(){
+        $('.socials').slideToggle();
+   }); 
+   $('#show-menu').on('click', function(){
+        $('.main-menu, .socials').slideToggle();
+   });
 })(jQuery);
--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,9 @@
+
 /* jQuery
 ============================================*/
 
 //= ../../bower_components/jquery/dist/jquery.min.js
 
-/* slick js
-====================================================*/
-
-//= ../../bower_components/slick-carousel/slick/slick.min.js
-
-/* validate 
-=========================================*/
-
-//= ../../bower_components/jquery-validation/dist/jquery.validate.min.js
-
-/* selectify 
-===================================*/
-
-//= ../../bower_components/select2/select2-4.0.3/dist/js/select2.min.js
-
-/* wow 
-==============================================*/
-//= ../../bower_components/wow/dist/wow.min.js
 
 /* custom.js
 ==========================================*/
